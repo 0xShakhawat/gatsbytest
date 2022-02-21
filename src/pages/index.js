46e4319be951b91906c9cc15bc24c09198f1acbf
Shakhawat Hossain <52792157+0xShakhawat@users.noreply.github.com>
2022-02-21 17:53:22 +0600

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,21 +11,17 @@ import {
 import { Layout, SEO } from "../components/common"
 
 const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi! I'm Shakhawat Hossain</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-
-  </Layout>
+  <>
+    <Layout>
+      <SEO title="Zonayed Ahmed" />
+      <Intro />
+      <About />
+      <Skill />
+      <LoveToDo />
+      <Portfolio />
+      <Contact />
+    </Layout>
+  </>
 )
 
 export default IndexPage
